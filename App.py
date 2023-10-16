76f4db739e677ff05d279abd6a1e640d09b78569
Xif <tmxif732@gmail.com>
2023-10-16 12:25:26 +0200

    👌 IMPROVE:  Add Hashlib

--- App.py
+++ App.py
@@ -1,27 +1,33 @@
+import hashlib
+
 class User:
-    pass
+    def __init__(self, id, name, password):
+        self.id = id
+        self.name = name
+        self.__salt = hashlib.sha256().hexdigest()[:20]
+        self.__password = self.__crypt_pwd(password)
 
-john = User()
+    def __crypt_pwd(self, password):
+        return hashlib.sha256((password + self.__salt).encode()).hexdigest()
 
-def user_check_pwd(user, password):
-    if user.password == password:
-        print('Password is correct.')
-        return True
-    else:
-        print('Password is incorrect.')
-        print('you typed {}.'.format(password))
-        return False
+    def user_check_pwd(self, password):
+        if self.password == password:
+            print('Password is correct.')
+            return True
+        else:
+            print('Password is incorrect.')
+            print(f'you typed {password} for user {self.name}')
+            print()
+            return False
 
-john.id = 1
-john.name = "John"
-john.password = "12345"
+john = User(1, "john", "12345")
 
-print('Hello, I am {}.'.format(john.name))
-print('My password is {}.'.format(john.password)) 
-print('My id is {}.'.format(john.id))
+print(f'Hello, I am {john.name}.')
+print(f'My password is {john.__password}.') 
+print(f'My id is {john.id}.')
 
 # del john.password
 # AttributeError: 'User' object has no attribute 'password'
 # print('My password is {}.'.format(john.password)) 
 
-user_check_pwd(john, '123465')
+john.user_check_pwd("12345")
